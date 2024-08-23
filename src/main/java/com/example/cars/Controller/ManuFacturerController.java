13b62fff2ae311ecab4bf87e1eab57fc2d67ef57
João Paulo <jpresendejava@gmail.com>
2024-08-23 10:34:56 -0400

    getOneManufacture

--- src/main/java/com/example/cars/Controller/ManuFacturerController.java
+++ src/main/java/com/example/cars/Controller/ManuFacturerController.java
@@ -1,12 +1,14 @@
 package com.example.cars.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,4 +38,13 @@ public class ManuFacturerController {
         return ResponseEntity.status(HttpStatus.OK).body(manuFacturerRepository.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneManufacture(@PathVariable(value = "id") Long id) {
+        Optional<Manufacturer> manufacO = manuFacturerRepository.findById(id);
+        if (manufacO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Manufacture não encontrado");
+
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(manufacO.get());
+    }
 }
