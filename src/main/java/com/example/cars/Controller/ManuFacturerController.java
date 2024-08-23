5d8b984c23a8a7c7fce52e512a7627cd7e7584cd
João Paulo <jpresendejava@gmail.com>
2024-08-23 11:11:41 -0400

    delete manufacture

--- src/main/java/com/example/cars/Controller/ManuFacturerController.java
+++ src/main/java/com/example/cars/Controller/ManuFacturerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,18 @@ public class ManuFacturerController {
         var manuModel = manufacO.get();
         BeanUtils.copyProperties(manufactureDto, manuModel);
         return ResponseEntity.status(HttpStatus.OK).body(manuFacturerRepository.save(manuModel));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteManufacture(@PathVariable(value = "id") Long id) {
+        Optional<Manufacturer> manufacO = manuFacturerRepository.findById(id);
+        if (manufacO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Manufacture não encontrado");
+
+        }
+        manuFacturerRepository.delete(manufacO.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Product delete successfully");
     }
 
 }
