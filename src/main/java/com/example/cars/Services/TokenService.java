e40e6a57d989d90b6e7e16e7c576fa9e3747fe43
João Paulo <jpresendejava@gmail.com>
2024-08-28 17:49:48 -0400

    erro jwt

--- src/main/java/com/example/cars/Services/TokenService.java
+++ src/main/java/com/example/cars/Services/TokenService.java
@@ -1,4 +1,55 @@
 package com.example.cars.Services;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.cars.Models.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gererateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("Login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.gererateExpirationDate())
+                    .sign(algorithm);
+            return token;
+
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while autenticating");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant gererateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
 }
