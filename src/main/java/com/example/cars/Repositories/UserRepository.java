786157fec653068379eb2db37eb6b3720e7c1f6a
João Paulo <jpresendejava@gmail.com>
2024-08-29 17:42:28 -0400

    customservice

--- src/main/java/com/example/cars/Repositories/UserRepository.java
+++ src/main/java/com/example/cars/Repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.cars.Repositories;
 
 import com.example.cars.Models.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+    Optional<User> findByEmail(String email);
 }
